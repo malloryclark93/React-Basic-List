9bb6409ef25224c030d4d8531be09a841b320a8c
malloryclark93 <mallory_clark@live.com>
2020-08-03 13:30:36 -0600

    working

---
+++ src/Components/Todoitem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    if(this.props.todo.completed) {
+      return {
+        textDecoration: 'line-through'
+      }
+    } else {
+      return {
+        textDecoration: 'none'
+      }
+    }
+  }
+  render() {
+    return (
+      <div style={this.getStyle()}>
+        <p>{ this.props.todo.title }</p>
+      </div>
+    )
+  }
+}
+
+// Proptypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+// const itemStyle = {
+//   backgroundColor: '#f4f4f4'
+// }
+
+export default TodoItem
